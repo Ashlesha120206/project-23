bb9c6ab8e745a3931f8405fb1397cdde429cd1eb
Ashlesha120206 <71136254+Ashlesha120206@users.noreply.github.com>
2020-11-07 17:53:12 +0530

    Add files via upload

---
+++ log.js
@@ -0,0 +1,24 @@
+class Log{
+ constructor(x, y, width, height){
+ var options = {
+     isStactic: true
+ } 
+
+   this.body = Bodies.rectangle(x, y, width, height, options);
+   this.width = width;
+   this.height = height; 
+   //World.add(world, this.body);
+  } 
+
+  display(){
+    var pos = this.body.position;
+    push();
+    translate(pos.x, pos.y);
+    rectMode(CENTER);
+    fill("red");
+    rect(0,0,this.width,this.height);
+    pop();
+  }
+
+
+}
